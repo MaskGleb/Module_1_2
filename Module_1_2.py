4df3c4737d55c33920fca06b033467efa40d8840
MaskGleb <gnk.g1o6us@gmail.com>
2024-12-21 23:54:04 +0500

    Add files via upload

--- Module_1_2.py
+++ Module_1_2.py
@@ -2,4 +2,4 @@ count_mission=int("12")
 count_hours=float("1.5")
 name_of_course=str("Python")
 one_lesson_time=count_hours/ count_mission
-print("Курс:",name_of_course,", всего задач: ",count_mission,", затрачено часов: ",count_hours,", среднее время выполнения:",one_lesson_time)
+print("Курс:",name_of_course,", всего задач:",count_mission,", затрачено часов:",count_hours,", среднее время выполнения:",one_lesson_time)
